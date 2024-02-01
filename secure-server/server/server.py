048aefbb37ce15a8645dbdb037dc3b4a24878792
Ali Bobi <alibobi@Alis-MacBook-Pro.local>
2024-02-01 13:21:24 -0500

    setup

---
+++ secure-server/server/server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route and a function to handle requests to that route
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+def build_db() -> None:
+    db.drop_all()
+    db.create_all()
+
+def run():
+	app.run(debug=True, port=9999)
+
+
+
