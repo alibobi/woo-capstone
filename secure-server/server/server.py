83a1acdfe9709fdc51855693aee17f13c0bda799
Ali Bobi <alibobi@Alis-MacBook-Pro.local>
2024-02-08 11:43:49 -0500

    frontend blueprint added

--- secure-server/server/server.py
+++ secure-server/server/server.py
@@ -1,23 +1,16 @@
 from flask import Flask, render_template
+from server.db import db
+from server.frontend_bp import frontend
 
 # Create a Flask application
 app = Flask(__name__)
-
-# Define a route and a function to handle requests to that route
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-# Define a route and a function to handle requests to that route
-@app.route('/test')
-def test():
-    return render_template('home.html')
 
 def build_db() -> None:
     db.drop_all()
     db.create_all()
 
 def run():
+	app.register_blueprint(frontend)
 	app.run(debug=True, port=9999)
 
 
