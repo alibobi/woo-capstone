8deb8b66fb0097a857c58f1b1f2cfe59227ca3b7
Paige Campbell <campbell.p@northeastern.edu>
2024-02-15 12:10:46 -0500

    rate limiting

--- secure-server/server/server.py
+++ secure-server/server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from server.db import db
 from server.frontend_bp import frontend
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # Create a Flask application
 app = Flask(__name__)
@@ -12,8 +14,14 @@ def build_db() -> None:
 def run():
 	app.secret_key = 'super secret key'
 	app.config['SESSION_TYPE'] = 'filesystem'
+ 
+	# initialize limiter
+	# don't need limiter var rn because not doing anything special
+	limiter = Limiter(
+		get_remote_address,
+		app=app,
+		default_limits=["5 per minute", "1 per second"]
+	)
+ 
 	app.register_blueprint(frontend)
 	app.run(debug=True, port=9999)
-
-
-
