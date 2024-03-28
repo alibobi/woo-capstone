e2117449b96788d9e0e44c1b09399f864b3b6229
alibobi <woo@woo-capstone.u2v4kgeoma2erf3xognadjemuf.bx.internal.cloudapp.net>
2024-03-28 15:20:26 +0000

    CSRF boom shakalaka

--- secure-server/server/configurations.py
+++ secure-server/server/configurations.py
@@ -5,5 +5,7 @@ POSTGRES_URI="localhost:9998"
 DATABASE_NAME="secureweb"
 DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_URI}/{DATABASE_NAME}"
 
+SECRET_KEY=b'4_2gz3uriq9piapdy;apl'
+
 class Config(object):
     APP_NAME = "Woo-Capstone"
