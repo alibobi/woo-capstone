6cef3db62bc8542e860d7d6d307d1faafebd40aa
Jaimie Griffin <34586152+griffinjaimie@users.noreply.github.com>
2024-04-04 12:51:14 -0400

    reset password functionality

--- secure-server/server/auth_bp.py
+++ secure-server/server/auth_bp.py
@@ -17,10 +17,57 @@ def configure_mfa():
     return render_template('configure_mfa.html', username=session["user"], secret=secret, qr_image=base64_qr_image)
 
 
-@auth.route("/change_password")
-@auth.route("/change_password.html")
-def change_password():
+@auth.route("/change_password", methods=["GET"])
+@auth.route("/change_password.html", methods=["GET"])
+def change_password_page():
     return render_template('change_password.html', username=session["user"])
+
+
+@auth.route("/change_password", methods=["POST"])
+@auth.route("/change_password.html", methods=["POST"])
+def change_password():
+    old_pw = request.form['old_pw']
+    new_pw = request.form['new_pw']
+    new_pw_conf = request.form['new_pw_conf']
+
+    # Get the current user's information 
+    user = User.query.filter_by(username=session["user"]).first()
+
+    # Ensure new password matches password rules and best practices
+    password_pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\_\-\!\@\#\$\%\^\&\*])[a-zA-Z0-9_\-\!\@\#\$\%\^\&\*]{8,}$')
+    if session["user"] in new_pw:
+        flash("Password cannot contain your username")
+        return render_template('change_password.html', username=session["user"])
+    else:
+        if password_pattern.match(new_pw):
+            print("Password well formatted")
+        else:
+            # Password must be 8+ characters
+            flash("Password must be 8+ characters and contain one lowercase letter, one uppercase letter, one digit, and one special character.")
+            return render_template('change_password.html', username=session["user"])
+ 
+    # Check that the user's old password entered matches the password currently set for the user account
+    # Helps to enforce security by making the user prove they know their password and so that an attacker
+    # can't change a user's password without having explicit knowledge of the current one 
+    # If the passwords don't match, throw an error 
+    if user and not verify_password(old_pw, user.password):
+        print("Logged in with user:", user) 
+        flash("Old password is incorrect")
+        return render_template('change_password.html', username=session["user"])
+    
+    if user and not (new_pw == new_pw_conf):
+        print("Logged in with user:", user) 
+        flash("New passwords do not match")
+        return render_template('change_password.html', username=session["user"])
+
+    if user and verify_password(old_pw, user.password) and (new_pw == new_pw_conf):
+        # SET USER'S NEW PASSWORD AND CHANGE IT IN DATABASE
+        new_passwd_hash = generate_password_hash(new_pw)
+        user.password = new_passwd_hash
+        db.session.commit()
+        return render_template('login_success.html', username=session["user"])
+    else:
+        return render_template('change_password.html', username=session["user"])
 
 
 @auth.route("/verify_2fa", methods=['GET'])
